0d415d80571fdde82f30dd18115b335bcef7f44f
Hector Lozano <hlozano2@cnm.edu>
2016-10-11 20:43:45 -0600

    Added html file

---
+++ public_html/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+
+		<!-- add CSS file -->
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css"/>
+		<title>HTML Sandbox</title>
+	</head>
+	<body>
+
+		<!-- this is the header -->
+		<header>
+			<h1>Hi! This is my page.</h1>
+		</header>
+
+		<!-- this is the main page content -->
+		<main>
+
+			<!-- begin section 1 -->
+
+			<section>
+				<p>
+					Hello and welcome to my personal web page! Allow me to introduce myself, My name is Hector Lozano. I am
+					a former critical care flight paramedic and navy corpsman.
+				</p>
+				<img src="img/" alt="femninja">
+			</section>
+
+			<!-- begin section 2 -->
+			<section>
+				<h2> My Favorite Things</h2>
+				<ul>
+					<li>Senator Arlo</li>
+					<li>Star Wars</li>
+					<li>Beer</li>
+					<li>Donuts</li>
+				</ul>
+			</section>
+
+			<!-- begin section 3-->
+			<section>
+				<table>
+					<tr>
+						<th>Column One</th>
+						<th>Column Two</th>
+					</tr>
+
+					<tr>
+						<td>Cat</td>
+						<td>Car</td>
+
+					</tr>
+
+					<tr>
+						<td>Dog</td>
+						<td>Bike</td>
+
+					</tr>
+
+					<tr>
+						<td>Star Wars</td>
+						<td>Wine</td>
+					</tr>
+				</table>
+			</section>
+		</main>
+	</body>
+</html>
